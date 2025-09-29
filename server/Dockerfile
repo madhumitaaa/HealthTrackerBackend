5d6be9da6ba176201252384ebc3afd7e0c22848d
madhumitaaa <madhumita.mandal.ee26@heritageit.edu.in>
2025-09-29 23:25:28 +0530

    added dockerfile

---
+++ server/Dockerfile
@@ -0,0 +1,7 @@
+FROM node:18-alpine
+WORKDIR /server
+COPY package*.json ./
+RUN npm install --production
+COPY . .
+EXPOSE 5000
+CMD ["npm","start"]
--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-// server.js (or app.js)
 
 const express = require('express');
 const mongoose = require('mongoose');
@@ -11,15 +10,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Health check endpoint
+
 app.get('/api/health', (req, res) => {
   res.status(200).json({ status: 'OK', message: 'Server is running!' });
 });
 
 app.use('/entries', entriesRoute);
-app.use('/auth', authRoute); // change from '/' to '/auth' for clarity
+app.use('/auth', authRoute); 
 
-const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 5000;
+
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+  .then(() => app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`)))
   .catch(err => console.error('MongoDB connection error:', err));
